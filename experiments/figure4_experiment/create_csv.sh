02aea8ac970eeb2d26cf699754329bab1d62afe9
Marcin Bodych <bodychmarcin@gmail.com>
2020-05-19 12:45:56 +0200

    renamed file with mean summaries

--- experiments/figure4_experiment/create_csv.sh
+++ experiments/figure4_experiment/create_csv.sh
@@ -5,7 +5,7 @@ PROJDIR=$PWD
 FIGURE_CSV=$PROJDIR/experiments/figure4_experiment/figure4.csv
 touch $FIGURE_CSV
 echo "Mean_Time;Mean_Affected;Wins_freq;c;c_norm;Init_people" > $FIGURE_CSV
-for i in $PROJDIR/experiments/figure4_experiment/figure*/outputs/under_critical/aggregated*/results.txt;
+for i in $PROJDIR/experiments/figure4_experiment/figure*/outputs/under_critical/aggregated*/results_mean.txt;
 do
   tail -n +2 $i >> $FIGURE_CSV
 done

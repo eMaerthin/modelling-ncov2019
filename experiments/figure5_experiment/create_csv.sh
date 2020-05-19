d78d9023445d636fdefc45d4e8bbe122d39e94b7
Marcin Bodych <bodychmarcin@gmail.com>
2020-05-19 12:27:57 +0200

    create_csv for figure5 added

--- experiments/figure5_experiment/create_csv.sh
+++ experiments/figure5_experiment/create_csv.sh
@@ -2,10 +2,10 @@
 
 PROJDIR=$PWD
 
-FIGURE_CSV=$PROJDIR/experiments/figure4_experiment/figure4.csv
+FIGURE_CSV=$PROJDIR/experiments/figure5_experiment/figure5.csv
 touch $FIGURE_CSV
-echo "Mean_Time;Mean_Affected;Wins_freq;c;c_norm;Init_people" > $FIGURE_CSV
-for i in $PROJDIR/experiments/figure4_experiment/figure*/outputs/under_critical/aggregated*/results.txt;
+echo "Last_processed_time;Total_#Affected;Total_#Detected;Total_#Deceased;Total_#Quarantined;c;c_norm;Init_#people;Prevalence_30days;Prevalence_60days;Prevalence_90days;Prevalence_120days;Prevalence_150days;Prevalence_180days;Band_hit_time;Subcritical;Prevalence_360days;runs;fear;detection_rate;increase_10;increase_20;increase_30;increase_40;increase_50;increase_100;increase_150;incidents_per_last_day;over_icu;hospitalized" > $FIGURE_CSV
+for i in $PROJDIR/experiments/figure5_experiment/figure*/outputs/figure5/aggregated*/results.txt;
 do
   tail -n +2 $i >> $FIGURE_CSV
 done
